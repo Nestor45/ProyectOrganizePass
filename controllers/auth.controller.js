0362edbb512e6b68f74d5c7f7242182c45806929
Nestor <nestoralvarez97@gmail.com>
2023-01-24 16:04:24 -0600

    Corrections: exception fix

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,24 +4,29 @@ import { uuid } from 'uuidv4';
 
 
 async function hashPassword(pwd) {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(pwd, salt);
-    return hashedPassword;
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(pwd, salt);
+        return hashedPassword;
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export const login = async (req, res) => {
     const data = req.body
-
-    const credentials = {
-        email: data.email,
-        password: data.password
+    
+    try {
+        const dataUser = await User.scan("email").contains(data.email).exec()
+        const match = await bcrypt.compare(data.password, dataUser[0].password);
+        if (!match) {
+            return res.status(400)
+        }
+        const dataInfo = {id:dataUser[0]._id, email:dataUser[0].email, name:dataUser[0].name}
+        return res.status(200).send(dataInfo)
+    } catch (error) {
+        console.log(error)
     }
-    const dataUser = await User.scan("email").contains(credentials.email).exec()
-    const match = await bcrypt.compare(credentials.password, dataUser[0].password);
-    if (!match) {
-        res.status(400).send(credentials)
-    }
-    res.status(200).send(credentials)
 }
 
 export const register = async (req, res) => {
@@ -30,17 +35,16 @@ export const register = async (req, res) => {
     try {
         const passwordHash = await hashPassword(password)
         const scan_test = await User.scan("email").contains(email).exec()
-        if (scan_test.count > 1) {
-            res.json({
+        if (scan_test.count >= 1) {
+            return res.json({
                 status: 'ok',
                 message: 'email invalid'
             })
-        } else {
-            const user = new User({_id:id, name, email, password:passwordHash, image})
-            console.log(user)
-            await user.save();
-            res.json({ok:id,name, email})
         }
+        const user = new User({_id:id, name, email, password:passwordHash, image})
+        console.log(user)
+        await user.save();
+        return res.json({ok:id,name, email})
     } catch (error) {
         console.log(error)
     }
